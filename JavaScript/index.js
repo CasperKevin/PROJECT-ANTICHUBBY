ba4e017f8edd5770d65d9bab22b0aaea558c00f7
Arazuki1337 <kfrog1222@gmail.com>
2025-07-14 08:52:03 +0700

    Arazuki-1

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,1 +1,21 @@
-
+//Banner slider functionality
+const slides = document.querySelectorAll(".slide");
+const prevButton = document.querySelector(".prev-slide");
+const nextButton = document.querySelector(".next-slide");
+let currentSlide = 0;
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.toggle("active", i === index);
+  });
+}
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}
+function prevSlide() {
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  showSlide(currentSlide);
+}
+nextButton.addEventListener("click", nextSlide);
+prevButton.addEventListener("click", prevSlide);
+showSlide(currentSlide);
